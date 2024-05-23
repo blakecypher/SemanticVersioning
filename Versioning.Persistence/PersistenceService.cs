8e0a5149c7ca494b41c7a5165b0c9e12ad217881
James Plaistere <james@jpdesign.co.uk>
2024-05-23 09:57:54 +0100

    Non-Functional changes and further documentation

--- Versioning.Persistence/PersistenceService.cs
+++ Versioning.Persistence/PersistenceService.cs
@@ -9,7 +9,10 @@ public class PersistenceService(IConfiguration configuration) : IPersistenceServ
     private readonly string? projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
     private readonly string? jsonContents = File.ReadAllText(configuration["ProjectDetails"] ?? string.Empty);
     
-    
+    /// <summary>
+    /// Retrieves project details asynchronously.
+    /// </summary>
+    /// <returns>The project details.</returns>
     public Task<ProjectDetails?> GetProjectDetails()
     {
         var semanticVersion = JsonSerializer.Deserialize<ProjectDetails>(jsonContents ?? string.Empty);
@@ -17,11 +20,20 @@ public class PersistenceService(IConfiguration configuration) : IPersistenceServ
         return Task.FromResult(semanticVersion);
     }
 
+    /// <summary>
+    /// Saves the project details to a JSON file.
+    /// </summary>
+    /// <param name="projectDetails">The project details to be saved.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     public Task SaveProjectDetails(ProjectDetails projectDetails)
     {
+        // Check if the project root is specified
         if (projectDirectory != null)
         {
+            // Construct the JSON file path
             var jsonFilePath = Path.Combine(projectDirectory, configuration["ProjectDetails"] ?? string.Empty);
+           
+            // Save the JSON file
             File.WriteAllText(jsonFilePath, JsonSerializer.Serialize(projectDetails, new JsonSerializerOptions
             {
                 WriteIndented = true
